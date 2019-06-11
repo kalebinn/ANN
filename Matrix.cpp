551d138d36135677652fc4b5fc01970f55eeb9ce
Kelvin <50221315+kalebinn@users.noreply.github.com>
2019-06-11 18:32:31 -0400

    Constructors(2/3) and Destructor implemented.

--- Matrix.cpp
+++ Matrix.cpp
@@ -11,22 +11,18 @@ namespace KR_Matrix {
 
 Matrix::Matrix(int rows, int cols)
 {
-	// TODO: write the constructor
+	// constructor
 	nRows = rows;
 	nCols = cols;
 	// allocate memory for nRows
 	A = new double *[nRows];
 	for (int i = 0; i < nRows; i++)
 	{
-		// each pointer element in nRow, will point to
+		// each pointer element in nRow, will point to a new column
 		A[i] = new double [nCols];
-	}
-
-	for (int i = 0; i < nRows; i++)
-	{
 		for (int j = 0; j < nCols; j++)
 		{
-			A[i][j] = 0;
+			A[i][j] = 0; // populate each element with 0
 		}
 	}
 }
@@ -36,15 +32,25 @@ Matrix::Matrix (char *input_file)
 	// TODO : constructor that reads array from input file
 }
 
-Matrix Matrix::dotProduct(Matrix arg)
+Matrix::Matrix(const Matrix &arg)
 {
-	// TODO: Apply dot product
-	Matrix Output(0,0);
+	nRows = arg.nRows;
+	nCols = arg.nCols;
+	// allocate memory for nRows
+	A = new double *[nRows];
+	for (int i = 0; i < nRows; i++)
+	{
+		// each pointer element in nRow, will point to another column
+		A[i] = new double [nCols]; // allocate memory for nCols
+		for (int j = 0; j < nCols; j++)
+		{
+			A[i][j] = arg.A[i][j]; // populate matrix
+		}
+	}
 
-	return Output;
 }
 
-Matrix Matrix::elementMult(Matrix arg)
+Matrix elementMult(const Matrix &Matrix1, const Matrix &Matrix2)
 {
 	// TODO: element wise multiplication of two matrices
 	Matrix Output(0,0);
@@ -52,21 +58,39 @@ Matrix Matrix::elementMult(Matrix arg)
 	return Output;
 }
 
-Matrix Matrix::operator = (Matrix arg)
+Matrix Matrix::operator = (const Matrix &arg)
 {
 	// TODO: overloaded operator =, assigns one matrix to another
 	Matrix Output(0,0);
 
 	return Output;
 }
-Matrix Matrix::operator + (Matrix arg)
+
+Matrix operator *(const Matrix &Matrix1, const Matrix &Matrix2)
+{
+	// TODO: Apply dot product
+	Matrix Output(0,0);
+
+	return Output;
+}
+
+Matrix operator *(double C, const Matrix &Matrix2)
+{
+	// TODO: Apply dot product
+	Matrix Output(0,0);
+
+	return Output;
+}
+
+Matrix operator + (const Matrix &Matrix1, const Matrix &Matrix2)
 {
 	// TODO: overloaded operator +, adds two matrices
 	Matrix Output(0,0);
 
 	return Output;
 }
-Matrix Matrix::operator - (Matrix arg)
+
+Matrix operator - (const Matrix &Matrix1, const Matrix &Matrix2)
 {
 	// TODO: overloaded operator -, subtracts two matrices
 	Matrix Output(0,0);
@@ -95,6 +119,7 @@ Matrix::~Matrix()
 		delete [] A[i];
 	}
 	delete [] A;
+
 }
 } /* namespace KR_Matrix */
 
--- Matrix.h
+++ Matrix.h
@@ -15,14 +15,16 @@ class Matrix
 {
 	public:
 		Matrix(int rows, int cols); // constructor
-		Matrix(char *input_file);
-		Matrix dotProduct(Matrix arg);
-		Matrix elementMult(Matrix arg);
-		Matrix operator= (Matrix arg);
-		Matrix operator+ (Matrix arg);
-		Matrix operator- (Matrix arg);
+		Matrix(char *input_file); // constructor that reads from file
+		Matrix(const Matrix &arg); // copy constructor
+		friend Matrix elementMult(const Matrix &Matrix1, const Matrix &Matrix2); // element by element mult
+		Matrix operator= (const Matrix &arg); // assignment operator
+		friend Matrix operator* (const Matrix &Matrix1, const Matrix &Matrix2); // dot product
+		friend Matrix operator* (double C, const Matrix &arg); // matrix multiplied by constant
+		friend Matrix operator+ (const Matrix &Matrix1, const Matrix &Matrix2); // matrix addition
+		friend Matrix operator- (const Matrix &Matrix1, const Matrix &Matrix2); // matrix subtraction
 		void printMatrix(void);
-		~Matrix(); // destructor
+		~Matrix(); // Destructor
 	private:
 		int nRows;
 		int nCols;
